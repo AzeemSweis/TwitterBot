51e214ff728fa40282ef16b39d245aa1e4c607cd
Azeem Sweis <azeemsw@gmail.com>
2021-01-04 16:31:01 -0800

    inital bot and config file plus Twit dependencies

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  consumer_key:         'QtcZMtfyvgcgCU2SjLbq5lI9v',
+  consumer_secret:      'DL7r4OuzzE59Fy1rU8FCtuVsW4UuTMmC6QYzUTabgojLlzS1lv',
+  access_token:         '1346239072784683008-ycZuMBD28xUFMMAHoqJLIwhNCUZ4xj',
+  access_token_secret:  'lutR3EloLhvxdJnryR0hpgYKUL59yEhL5PFH6W02CvoEh'
+  //timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+  //strictSSL:            true,     // optional - requires SSL certificates to be valid.
+}
